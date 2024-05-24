2054bb686c171a5c2370f0871183dc88cce71c6b
muni <muni@gmail.com>
2024-05-24 10:35:54 +0530

    hello

---
+++ 3shell.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+
+Person1=Ramaesh
+Person2=Gopi
+
+
+echo "$Person1: Hai $Person2 , how are you "
+echo " $Person2: Hai $Person1 , iam fine how are you "
+echo " $Person1: i am good, now we are starting to learn Aws DevOps "
+echo " $Person2: ok $Person1 , lets start"
