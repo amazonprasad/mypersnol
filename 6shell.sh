860c5d4390099d93d34b60694e6415a43933f850
muni <muni@gmail.com>
2024-05-24 10:55:53 +0530

    hello

---
+++ 6shell.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo " please enter yourname"
+
+read -s username
+
+echo " username is: $username"
+echo " please enter your password"
+read -s password
+echo " password is: $password"
+
