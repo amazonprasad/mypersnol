d8d4d76ff06e34da65ad2fb289474eddddae13dc
muni <muni@gmail.com>
2024-05-24 10:31:22 +0530

    hello

---
+++ 2shell.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+echo "Ramesh: Hai Gopi , how are you "
+echo " Gopi: Hai Ramesh , iam fine how are you "
+echo " Ramesh: i am good, now we are starting to learn Aws DevOps "
+echo " Gopi: ok Ramesh , lets start"
+
+
