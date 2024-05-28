1b1696f30cc619a203b11788d393032eb1a3177b
muni <muni@gmail.com>
2024-05-28 18:05:00 +0530

    gt or not script

--- 8-print the number gt or not.sh
+++ 8-print the number gt or not.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
 number=$1
+ 
+R="\e[31m"
+G="\e[32m"
+N="\e[0m"
 if [ $1 -gt 100 ]
 then
-     echo " $number is gt 100"
+     echo -e  " $R $number is gt 100 $N"
 else 
-     echo " $number is not gt 100"
+     echo -e "$G $number is not gt 100 $N"
 
 fi 
